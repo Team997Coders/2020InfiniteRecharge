52400bff07a03d136c287cfe37096c6cd50075c7
TimothyDjang2 <timothydjang@gmail.com>
2020-01-07 17:12:12 -0800

    Origin project

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,22 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ * Add your docs here.
+ */
+public class OI {
+
+    private XboxController gamepad1;
+
+    public OI() {
+        gamepad1 = new XboxController(0);
+    }
+}
