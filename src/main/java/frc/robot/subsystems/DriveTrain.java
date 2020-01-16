624bf8c3b8bc9e7dbf441e4b6e12b414f96867b0
TimothyDjang2 <timothydjang@gmail.com>
2020-01-15 17:38:51 -0800

    did smart dashboard and sensors

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -2,40 +2,45 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.commands.ArcadeDrive;
 
 public class DriveTrain extends SubsystemBase {
   
-  TalonFX frontLeft;
-  TalonFX frontRight;
-  TalonFX backLeft;
-  TalonFX backRight;
-  SupplyCurrentLimitConfiguration currentLimitConfig;
-  
+  private TalonFX frontLeft;
+  private TalonFX frontRight;
+  private TalonFX backLeft;
+  private TalonFX backRight;
+
   private DriveTrain() {
     frontLeft = new TalonFX(Constants.Ports.motorFrontLeft);
     frontRight = new TalonFX(Constants.Ports.motorFrontRight);
     backLeft = new TalonFX(Constants.Ports.motorBackLeft);
     backRight = new TalonFX(Constants.Ports.motorBackRight);
 
-    currentLimitConfig = new SupplyCurrentLimitConfiguration(true, 40, 50, 0.1);
+    SupplyCurrentLimitConfiguration currentLimitConfig = new SupplyCurrentLimitConfiguration(true, 40, 50, 0.1);
     frontLeft.configSupplyCurrentLimit(currentLimitConfig, 10);
     frontRight.configSupplyCurrentLimit(currentLimitConfig, 10);
     backLeft.configSupplyCurrentLimit(currentLimitConfig, 10);
     backRight.configSupplyCurrentLimit(currentLimitConfig, 10);
+
+    frontLeft.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 10);
+    frontLeft.setSelectedSensorPosition(0, 0, 10);
+    frontRight.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 10);
+    frontRight.setSelectedSensorPosition(0, 0, 10);
+
 
     backLeft.follow(frontLeft);
     backRight.follow(frontRight);
 
     /**
      * TODO:
-     * 1) Reverse one side
-     * 2) Add current limiting
-     * 3) Add methods to get encoder position and velocities (And maybe convert them to useful units)
+     * 3) Add methods to get encoder velocities (And maybe convert them to useful units)
      * 4) Put encoder readings on SmartDashboard
      */
 
@@ -43,13 +48,26 @@ public class DriveTrain extends SubsystemBase {
   }
 
   public void setMotors(double leftSpeed, double rightSpeed) {
-    frontLeft.set(ControlMode.PercentOutput, leftSpeed);
+    frontLeft.set(ControlMode.PercentOutput, -leftSpeed);
     frontRight.set(ControlMode.PercentOutput, rightSpeed);
+  }
+
+  public double getLeftSensor() {
+    return frontLeft.getSelectedSensorPosition(0);
+  }
+
+  public double getRightSensor() {
+    return frontRight.getSelectedSensorPosition(0);
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Drivetrain/Front Right Motor Position", getRightSensor());
+    SmartDashboard.putNumber("Drivetrain/Front Left Motor Position", getLeftSensor());
+
+    SmartDashboard.putNumber("Drivetrain/Left Motor Velocity", frontLeft.getSelectedSensorVelocity(0));
+    SmartDashboard.putNumber("Drivetrain/Right Motors Velocity", frontRight.getSelectedSensorVelocity(0));
+    
   }
 
   private static DriveTrain instance;
