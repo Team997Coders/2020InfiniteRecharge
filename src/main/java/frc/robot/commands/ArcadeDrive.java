3f85379f63602928c0669ceaa9dd111adc85bfe2
TimothyDjang2 <timothydjang@gmail.com>
2020-01-17 17:48:15 -0800

    merged driveTrain into dev

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -9,7 +9,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
-import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.Drivetrain;
 
 public class ArcadeDrive extends CommandBase {
   /**
