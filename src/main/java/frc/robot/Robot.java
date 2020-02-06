f0582007f4bd5579032054f983cbe181167dcd77
Robotics <hunter.barclay18@gmail.com>
2020-02-05 17:19:11 -0800

    Made sure I know how to write camera code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.team997coders.spartanlib.limelight.LimeLight;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.*;
@@ -28,6 +29,8 @@ public class Robot extends TimedRobot {
   
   @Override
   public void robotInit() {
+    //CameraServer.getInstance().startAutomaticCapture(0);
+
     CommandScheduler.getInstance().cancelAll();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
