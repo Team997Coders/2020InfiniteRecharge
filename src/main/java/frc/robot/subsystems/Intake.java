813f3d9c99673b8dfb94449669a7509420503287
Rohan Pankaj <rohansp01@gmail.com>
2020-03-12 22:42:10 -0700

    Added documentation for the Shooter and Hopper

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+//imports 
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -20,67 +21,82 @@ import frc.robot.Robot;
 
 public class Intake extends SubsystemBase {
 
+  //Create hardware objects 
   private Solenoid intakePiston;
   private CANSparkMax mLeader, mFollower;
   private CANEncoder encoder;
 
   private Intake() {
+
+    //crete instances of mLeader, mFollower, and the intakePiston and set their port and in the case of the motors, their motor type 
     mLeader = new CANSparkMax(Constants.Ports.INTAKE_MOTOR_1, MotorType.kBrushless);
     mFollower = new CANSparkMax(Constants.Ports.INTAKE_MOTOR_2, MotorType.kBrushless);
     intakePiston = new Solenoid(Constants.Ports.INTAKE_SOLENOID);
 
+    //restore the facotry defaults for the motors so that there are no conflicing pre-set values
     mLeader.restoreFactoryDefaults();
     mFollower.restoreFactoryDefaults();
 
+    // set mFolloer to follow mLeader
     mFollower.follow(mLeader, true);
 
+    //set the encoder so that it gets its information from the mLeader encoder
     encoder = mLeader.getEncoder();
 
+    //set the default state for the intake piston to be retracted 
     intakePiston.set(false);
 
-    mLeader.setSmartCurrentLimit(50);
-    mLeader.setIdleMode(IdleMode.kCoast);
-    mFollower.setSmartCurrentLimit(50);
-    mFollower.setIdleMode(IdleMode.kCoast);
+    mLeader.setSmartCurrentLimit(50); // current limit on the intake motor is 50%
+    mLeader.setIdleMode(IdleMode.kCoast); // when not in use set the mLeader motor to coast
+    mFollower.setSmartCurrentLimit(50); // current limit on the intake motor is 50%
+    mFollower.setIdleMode(IdleMode.kCoast); // when not in use set the mLeader motor to coast
 
-    register();
+    register(); // save object referenecs
   }
 
+  // make an instance of the Intake if it dossn't exsist
+  private static Intake instance;
+  public static Intake getInstance() { if (instance == null) instance = new Intake(); return instance; }
+
+  // set the percent output for the mLeader motor
   public void setPercent(double percent) {
     mLeader.set(percent);
   }
 
+  // get the RPM of the mLeader motor through the encoder
   public double getRPM() {
     return encoder.getVelocity();
   }
  
+  //Toggle the piston between the retracted and extended states 
   public void togglePiston() {
-    intakePiston.set(!intakePiston.get());
+    intakePiston.set(!intakePiston.get()); // set the piston state to the opposite of its current state
   }
 
+  // actuate the Pistion to a given state
   public void setPiston(boolean extend) {
     intakePiston.set(extend);
   }
 
+  //get the current state of the Piston
   public boolean getPiston() {
     return intakePiston.get();
   }
 
-  @Override
-  public void periodic() {
-    updateSmartDashboard();
-  }
-
+  // Update the smart Dahsboard
   public void updateSmartDashboard() {
-    // if you put anything here make sure to register the subsystem.
+   
     
-    SmartDashboard.putNumber("Intake/RPMs", getRPM());
+    SmartDashboard.putNumber("Intake/RPMs", getRPM()); // put the intake motor RPM's on the dashbaord
 
-    if (Robot.verbose) {
-      // put non-essential data here.
+    if (Robot.verbose) { // if verbose (debugging) turned on run the below code
+    
     }
   }
 
-  private static Intake instance;
-  public static Intake getInstance() { if (instance == null) instance = new Intake(); return instance; }
+  @Override
+  public void periodic() {
+    updateSmartDashboard(); // update the smart dashboard on every cycle
+  }
+
 }
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+// Imports 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANEncoder;
@@ -14,74 +15,93 @@ import frc.robot.Robot;
 
 public class Shooter implements Subsystem {
 
+  // Craete Hardwware Objects
   private CANSparkMax mMotor1, mMotor2;
   private CANPIDController mController;
   private CANEncoder mEncoder;
 
   private Shooter() {
+
+    //create an instance of the mMotor1 and mMotor2 objects and set their ports and MotorType
     mMotor1 = new CANSparkMax(Constants.Ports.SHOOTER_MOTOR_1, MotorType.kBrushless);
     mMotor2 = new CANSparkMax(Constants.Ports.SHOOTER_MOTOR_2, MotorType.kBrushless);
 
+    //Restore the Factory defaults to make sure that there are no pre-set settings
     mMotor1.restoreFactoryDefaults();
     mMotor2.restoreFactoryDefaults();
 
+    //When not in use set the motors to coase
     mMotor1.setIdleMode(IdleMode.kCoast);
     mMotor2.setIdleMode(IdleMode.kCoast);
 
     //mMotor1.setInverted(true);
     //mMotor2.setInverted(true);
 
+    //set mMotor2 to follow mMotor1
     mMotor2.follow(mMotor1);
 
-    mEncoder = mMotor1.getEncoder();
-    mEncoder.setVelocityConversionFactor(Constants.Values.SHOOTER_GEARING);
+    //get the mMotor1 encoder value
+    mEncoder = mMotor1.getEncoder(); // get the value of the encoder on mMotor1
+    mEncoder.setVelocityConversionFactor(Constants.Values.SHOOTER_GEARING); // convert the encoder value to a velocity based on the shooter gearing
 
-    mController = mMotor1.getPIDController();
-    mController.setP(Constants.Values.SHOOTER_VELOCITY_GAINS.kP);
-    mController.setI(Constants.Values.SHOOTER_VELOCITY_GAINS.kI);
-    mController.setD(Constants.Values.SHOOTER_VELOCITY_GAINS.kD);
-    mController.setFF(Constants.Values.SHOOTER_VELOCITY_GAINS.kF);
 
-    register();
+    //PID controller
+    mController = mMotor1.getPIDController(); // create the PIT controller
+    mController.setP(Constants.Values.SHOOTER_VELOCITY_GAINS.kP); // set the Proportional value
+    mController.setI(Constants.Values.SHOOTER_VELOCITY_GAINS.kI); // set the Integral value
+    mController.setD(Constants.Values.SHOOTER_VELOCITY_GAINS.kD); // set the Derivative value
+    mController.setFF(Constants.Values.SHOOTER_VELOCITY_GAINS.kF); // set the Fixed value
+
+    register(); // save object references
   }
 
+  //If there is no instance of the shooter create one
   private static Shooter instance;
   public static Shooter getInstance() {if(instance == null) instance = new Shooter(); return instance;}
 
+  //Set the RPM of the shooter through the PID controller
   public void setRPM(double RPM) {
     mController.setReference(RPM, ControlType.kVelocity);
   }
 
+  // Set the percent output of the lead shooter motor
   public void SetYeeterPercent(double perc) {
     mMotor1.set(perc);
   }
 
+  // stop the shooter motor
   public void GoodStop() {
     mMotor1.set(0.0);
   }
 
-  public void updateSmartDashboard(){
-    SmartDashboard.putNumber("Shooter/encoderspeed", getRPMs());
-    if (Robot.verbose) {
-      SmartDashboard.putNumber("Shooter/Ball Ejection Speed", getBallSpeed());
-    }
-  }
-
+ 
+  // get the RPM's of the shooter
   public double getRPMs() {
-    return mEncoder.getVelocity();
+    return mEncoder.getVelocity(); // get the velocity from the encoder 
   }
 
+  // get the needed ball velocity to make it into the top target given a supplied distance
+  // TODO: Find The units for this
   public double getNeededBallVelocity(double distance) {
     return (1 / (((Math.tan(Constants.Values.SHOOTER_RELEASE_ANGLE * (Math.PI / 180))) / (-4.9 * distance)) + ((2.49 - Constants.Values.SHOOTER_RELEASE_HEIGHT) / (4.9 * distance * distance)))) * (1 / Math.cos(Constants.Values.SHOOTER_RELEASE_ANGLE * (Math.PI / 180)));
   }
 
+  //From the motor velocity, calculate the velocity of the ball
   public double getBallSpeed() {
     return (getRPMs() / 60) * (Constants.Values.SHOOTER_CIRCUMFERENCE_CM / 100);
   }
 
+  //update the smartdashboard
+  public void updateSmartDashboard(){
+    SmartDashboard.putNumber("Shooter/encoderspeed", getRPMs()); //put the encoder spped on the smart dashboard
+    if (Robot.verbose) { // if verbose (debugging) turned on run the following
+      SmartDashboard.putNumber("Shooter/Ball Ejection Speed", getBallSpeed()); // put the ball ejection spped on the smart dahsboard
+    }
+  }
+
   @Override
   public void periodic() {
-    updateSmartDashboard();
+    updateSmartDashboard(); // update the smart dashboard every cycle 
   }
 
 }
